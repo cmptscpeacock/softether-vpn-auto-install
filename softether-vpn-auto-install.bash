696f1954548a693ed0fe816e79b00278619ffa1c
Chris Peacock <chris.peacock@cmptechsol.co.uk>
2019-09-30 18:51:15 +0100

    created dnsmasq.conf

--- softether-vpn-auto-install.bash
+++ softether-vpn-auto-install.bash
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Define console colors
+# define console colors
+
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-# Execute as sudo
+# execute as sudo
+
 (( EUID != 0 )) && exec sudo -- "$0" "$@"
 clear
 
-# REMOVE PREVIOUS INSTALL
-# Check for SE install folder
+# remove previous versions
+## check for SE install folder
+
 if [ -d "/opt/vpnserver" ]; then
   rm -rf /opt/vpnserver > /dev/null 2>&1
 fi
@@ -18,23 +21,29 @@ if [ -d "/tmp/softether-autoinstall" ]; then
   rm -rf /tmp/softether-autoinstall > /dev/null 2>&1
 fi
 
-# Check for init script
+## check for init script
+
 if
   [ -f "/etc/init.d/vpnserver" ]; then rm /etc/init.d/vpnserver;
 fi
 
-# Remove vpnserver from systemd
+## remove vpnserver from systemd
+
 update-rc.d -f vpnserver remove
 
-# Create working directory
+# install SE
+## create working directory
+
 mkdir -p /tmp/softether-autoinstall
 cd /tmp/softether-autoinstall
 
-# Perform apt update
+## Perform apt update
+
 ##apt update -y
 ##apt upgrade -y
 
-# Install build-essential
+## install build-essential
+
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' build-essential|grep "install ok installed")
 echo  "Checking for build-essential: $PKG_OK"
 if [ "" == "$PKG_OK" ]; then
@@ -42,7 +51,8 @@ if [ "" == "$PKG_OK" ]; then
   sudo apt install -y build-essential
 fi
 
-# Download SoftEther
+## download SoftEther
+
 printf "\nDownloading SoftEther"
 wget https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.29-9680-rtm/softether-vpnserver-v4.29-9680-rtm-2019.02.28-linux-arm_eabi-32bit.tar.gz
 tar xzf softether-vpnserver-v4.29-9680-rtm-2019.02.28-linux-arm_eabi-32bit.tar.gz
@@ -52,6 +62,8 @@ cd /tmp/softether-autoinstall && mv vpnserver/ /opt
 chmod 600 /opt/vpnserver/* && chmod 700 /opt/vpnserver/vpncmd && chmod 700 /opt/vpnserver/vpnserver
 cd /tmp/softether-autoinstall
 
+## configure dhcp service
+
 apt install -y dnsmasq
 wget -O dnsmasq.conf https://raw.githubusercontent.com/icoexist/softether-autoinstall/master/dnsmasq.conf
 rm /etc/dnsmasq.conf && mv dnsmasq.conf /etc/dnsmasq.conf
@@ -59,7 +71,9 @@ wget -O dnsmasq.service https://raw.githubusercontent.com/cmptscpeacock/softethe
 mv dnsmasq.service /lib/systemd/system/dnsmasq.service
 wget -O resolved.conf https://raw.githubusercontent.com/cmptscpeacock/softether-vpn-auto-install/master/resolved.conf
 mv resolved.conf /etc/systemd/resolved.conf
+sudo service systemd-resolved restart
 
+## configure SE as a local bridge
 
 wget -O vpnserver-init-bridge https://raw.githubusercontent.com/icoexist/softether-autoinstall/master/vpnserver-init-bridge > /dev/null 2>&1
 mv vpnserver-init-bridge /etc/init.d/vpnserver
@@ -74,3 +88,9 @@ cd && rm -rf /tmp/softether-autoinstall > /dev/null 2>&1
 systemctl is-active --quiet vpnserver && echo "Service vpnserver is running."
 ##printf "\n${RED}!!! IMPORTANT !!!${NC}\n\nTo configure the server, use the SoftEther VPN Server Manager located here: http://bit.ly/2D30Wj8 or use ${RED}sudo /opt/vpnserver/vpncmd${NC}\n\n${RED}!!! UFW is not enabled with this script !!!${NC}\n\nTo see how to open ports for SoftEther VPN, please go here: http://bit.ly/2JdZPx6\n\nNeed help? Feel free to join the Discord server: https://icoexist.io/discord\n\n"
 ##printf "\n${RED}!!! IMPORTANT !!!${NC}\n\nYou still need to add the local bridge using the SoftEther VPN Server Manager. It is important that after you add the local bridge, you restart both dnsmasq and the vpnserver!\nSee the tutorial here: http://bit.ly/2HoxlQO\n\n"
+
+
+
+cd /usr/local/vpnserver/
+sudo ./vpncmd
+echo $'1\n\n' 
