d13c067bde84e14b8f5b01f639105bf058a567d7
Chris Peacock <chris.peacock@cmptechsol.co.uk>
2019-10-01 16:36:19 +0100

    updated user prompts

--- softether-vpn-auto-install.bash
+++ softether-vpn-auto-install.bash
@@ -2,6 +2,8 @@
 
 # variables
 
+seInstallFileUrl='https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.29-9680-rtm/softether-vpnserver-v4.29-9680-rtm-2019.02.28-linux-arm_eabi-32bit.tar.gz'
+seInstallFileName='softether-vpnserver-v4.29-9680-rtm-2019.02.28-linux-arm_eabi-32bit.tar.gz'
 hubName='hubName1'
 
 ## credentials
@@ -18,6 +20,24 @@ NC='\033[1;37m' # No Color
 UNDERLINE='\033[4m'
 RESETUNDERLINE='\033[24m'
 
+# user confirmation
+
+printf "\n${RED}${UNDERLINE}IMPORTANT${NC}${RESETUNDERLINE}\n\nSoftEther VPN is about to be uninstalled. Backup your config file via the GUI or copy it from /opt/vpnserver/ if you are upgrading. \n\n"
+
+while true; do
+    read -p "Reboot needed prior to uninstallation. Have you rebooted? (Y - will continue | N - will reboot now)?" answer
+    case $answer in
+        [yY][eE][sS]|[yY]* )
+            break;;
+        [Nn][Oo]|[Nn]* ) 
+            echo "Rebooting...";
+            sleep 3 
+            reboot;;
+        * ) 
+            echo "Please answer y|yes or n|no.";;
+    esac
+done
+
 # execute as sudo
 
 (( EUID != 0 )) && exec sudo -- "$0" "$@"
@@ -27,10 +47,22 @@ clear
 ## remove hub and bridge
 ## password command omitted so manually entered
 
+printf "\n${RED}${UNDERLINE}Previous Password${NC}${RESETUNDERLINE} If asked, enter the previous Hub password \n\n"
+
 if [ -d "/opt/vpnserver" ]; then
   cd /opt/vpnserver/
   ./vpncmd /server localhost:443 /cmd HubDelete ${hubName}
   ./vpncmd /server localhost:443 /cmd bridgeDelete ${hubName} /Device:soft1
+fi
+
+## stop vpnserver
+
+systemctl stop vpnserver
+
+## delete config file
+
+if [ -f "/opt/vpnserver/vpn_server.config" ]; then
+  rm -f /opt/vpnserver/vpn_server.config > /dev/null 2>&1
 fi
 
 ## check for SE install folder
@@ -76,8 +108,8 @@ fi
 ## download SoftEther VPN
 
 printf "\nDownloading SoftEther"
-wget https://github.com/SoftEtherVPN/SoftEtherVPN_Stable/releases/download/v4.29-9680-rtm/softether-vpnserver-v4.29-9680-rtm-2019.02.28-linux-arm_eabi-32bit.tar.gz
-tar xzf softether-vpnserver-v4.29-9680-rtm-2019.02.28-linux-arm_eabi-32bit.tar.gz
+wget ${seInstallFileUrl}
+tar xzf ${seInstallFileName}
 cd vpnserver
 echo $'1\n1\n1' | make &&
 cd /tmp/softether-autoinstall && mv vpnserver/ /opt
@@ -89,18 +121,19 @@ cd /tmp/softether-autoinstall
 apt install -y dnsmasq
 wget -O dnsmasq.conf https://raw.githubusercontent.com/cmptscpeacock/softether-vpn-auto-install/master/dnsmasq.conf
 rm /etc/dnsmasq.conf && mv dnsmasq.conf /etc/dnsmasq.conf
-##chmod 644 /etc/dnsmasq.conf
 wget -O dnsmasq.service https://raw.githubusercontent.com/cmptscpeacock/softether-vpn-auto-install/master/dnsmasq.service
 mv dnsmasq.service /lib/systemd/system/dnsmasq.service
-##chmod 644 /lib/systemd/system/dnsmasq.service
 wget -O resolved.conf https://raw.githubusercontent.com/cmptscpeacock/softether-vpn-auto-install/master/resolved.conf
 mv resolved.conf /etc/systemd/resolved.conf
-##chmod 644 /etc/systemd/resolved.conf
 sudo service systemd-resolved restart
+
 ## enable ipv4 forwarder
+
 wget -O ipv4_forwarding.conf https://raw.githubusercontent.com/cmptscpeacock/softether-vpn-auto-install/master/ipv4_forwarding.conf
 mv ipv4_forwarding.conf /etc/sysctl.d/ipv4_forwarding.conf
+
 ## set persistent tap interface
+
 wget -O interfaces https://raw.githubusercontent.com/cmptscpeacock/softether-vpn-auto-install/master/interfaces
 mv interfaces /etc/network/interfaces
 sysctl --system
@@ -118,30 +151,30 @@ systemctl restart dnsmasq
 printf "\nCleaning up...\n\n"
 cd && rm -rf /tmp/softether-autoinstall > /dev/null 2>&1
 systemctl is-active --quiet vpnserver && echo "Service vpnserver is running."
+sleep 10
 
 # confifgure SE
 
 ## generate credentials + randoms
+seServerPassword=$(cat /dev/urandom | base64 -w 0 | fold -w 8 | head -1)
 hubPassword=$(cat /dev/urandom | base64 -w 0 | fold -w 8 | head -1)
 adminPassword=$(cat /dev/urandom | base64 -w 0 | fold -w 8 | head -1)
 ddnsHostname=$(cat /dev/urandom | base64 -w 0 | tr -d '[A-Z/+]' | fold -w 8 | head -1)
 preSharedKey=$(cat /dev/urandom | base64 -w 0 | tr -d '[/+]' | fold -w 9 | head -1)
 
-## create bridge and hub
+## set dir
 
 cd /opt/vpnserver/
-./vpncmd /server localhost:443 /cmd bridgecreate ${hubName} /Device:soft1 /TAP:yes
-##./vpncmd /server localhost:443 /cmd HubCreate ${hubName} /PASSWORD:${hubPassword}
-./vpncmd /server localhost:443 /cmd HubCreate ${hubName} /PASSWORD:1234
 
+## create bridge and hub
+
+./vpncmd /server localhost:443 /cmd bridgecreate ${hubName} /Device:soft1 /TAP:yes
+./vpncmd /server localhost:443 /cmd HubCreate ${hubName} /PASSWORD:${hubPassword}
 
 ## create default username
 
-##./vpncmd /server localhost:443 /hub:${hubName} /PASSWORD:${hubPassword} /CMD UserCreate ${hubAdmin} /GROUP:none /REALNAME:none /NOTE:none
-./vpncmd /server localhost:443 /hub:${hubName} /PASSWORD:1234 /CMD UserCreate ${hubAdmin} /GROUP:none /REALNAME:none /NOTE:none
-##./vpncmd /server localhost:443 /hub:${hubName} /PASSWORD:${hubPassword} /CMD UserPasswordSet ${hubAdmin} /PASSWORD:${adminPassword}
-./vpncmd /server localhost:443 /hub:${hubName} /PASSWORD:1234 /CMD UserPasswordSet ${hubAdmin} /PASSWORD:123456789
-
+./vpncmd /server localhost:443 /hub:${hubName} /PASSWORD:${hubPassword} /CMD UserCreate ${hubAdmin} /GROUP:none /REALNAME:none /NOTE:none
+./vpncmd /server localhost:443 /hub:${hubName} /PASSWORD:${hubPassword} /CMD UserPasswordSet ${hubAdmin} /PASSWORD:${adminPassword}
 
 ## create vpn
 
@@ -149,6 +182,13 @@ cd /opt/vpnserver/
 ./vpncmd /server localhost:443 /cmd DynamicDnsSetHostname ${ddnsHostname}
 ./vpncmd /server localhost:443 /cmd VpnAzureSetEnable yes
 
+## set server password
+
+./vpncmd /server localhost:443 /cmd ServerPasswordSet ${seServerPassword}
+
+## output details
+
+printf "\n${RED}${UNDERLINE}SE Server Password:${NC}${RESETUNDERLINE}  ${seServerPassword}\n\n"
 printf "\n${RED}${UNDERLINE}Hub Password:${NC}${RESETUNDERLINE}  ${hubPassword}\n\n"
 printf "\n${RED}${UNDERLINE}Admin Username:${NC}${RESETUNDERLINE}  ${hubAdmin} ${RED}${UNDERLINE}Admin Password:${NC}${RESETUNDERLINE}  ${adminPassword}\n\n"
 printf "\n${RED}${UNDERLINE}IPSec Pre-Shared Key:${NC}${RESETUNDERLINE}  ${preSharedKey}\n\n"
